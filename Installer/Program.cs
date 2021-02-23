f979358c5be6f6bbd3e7450c1c1c0c1380c99315
PoofyFox <Poof@foxmanager.pw>
2021-02-22 19:55:41 -0500

    Committing progress and Tasklist.

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 
 namespace Installer {
     class Program {
+        // TODO: I still have to search in predictive file locations before scanning all drives.
+        // TODO: Fix scanner console output.
         static void Main() {
             Console.WriteLine("Scanning for ChilloutVR...");
             var gameLocation = GameScanner.FindSteamGame("ChilloutVR.exe")
